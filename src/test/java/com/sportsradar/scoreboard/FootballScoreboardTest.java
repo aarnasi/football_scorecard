4f3c61ac4c06ad3e2612cb0f4e814162be105fb5
aarnasi <aarna.si09@gmail.com>
2022-08-04 00:46:19 +0530

    Adds test cases related to football scoreboard operations.
    The supported football scoreboard operations include: starting a game, ending a game, updating score, generating summary for games.

---
+++ src/test/java/com/sportsradar/scoreboard/FootballScoreboardTest.java
@@ -0,0 +1,37 @@
+package com.sportsradar.scoreboard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FootballScoreboardTest {
+
+    @Test
+    void shouldStartGame_success(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldFinishGame_success(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldUpdateScore_success(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldProduceSummary_success(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldFinishGame_gameDoesNotExist_throws(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldUpdateScore_gameDoesNotExist_throws(){
+        assertEquals(true,false);
+    }
+    @Test
+    void shouldUpdateScore_negativeScoreInput_failure(){
+        assertEquals(true,false);
+    }
+}
